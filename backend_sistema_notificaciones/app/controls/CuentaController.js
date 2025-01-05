ad04adb8c9ba7e4905576005709134780a29977b
luisNarvaez20 <luis.a.narvaez@unl.edu.ec>
2025-01-05 00:27:40 -0500

    feature: add controls usuario y cuenta

---
+++ backend_sistema_notificaciones/app/controls/CuentaController.js
@@ -0,0 +1,82 @@
+'use strict';
+const { body, validationResult, check } = require('express-validator');
+var models = require('../models/');
+var usuario = models.usuario;
+var cuenta = models.cuenta;
+
+const bcypt = require('bcrypt');
+let jwt = require('jsonwebtoken');
+
+class CuentaController {
+
+    async sesion(req, res) {
+        let errors = validationResult(req);
+        if (errors.isEmpty()) {
+            var login = await cuenta.findOne({
+                where: { user: req.body.user },
+                include: [
+                    {
+                        model: usuario,
+                        as: 'usuario',
+                        attributes: ['apellidos', 'nombres', 'external_id'],
+                    }
+                ]
+            });            
+            if (login === null) {
+                res.status(400);
+                res.json({
+                    msg: "CUENTA NO ENCONTRADA",
+                    code: 400
+                });
+            } else {
+                res.status(200);
+                var isClaveValida = function (clave, claveUser) {
+                    return bcypt.compareSync(claveUser, clave);
+                }
+                if (login.estado == true) {
+                    if (isClaveValida(login.clave, req.body.clave)) { //login.clave---BD //req.body.clave---lo que manda el correo
+                        const tokenData = {
+                            external: login.external_id,
+                            user: login.user,
+                            check: true
+                        };
+                        require('dotenv').config();
+                        const llave = process.env.KEY_SQ;
+                        const token = jwt.sign(tokenData, llave, {
+                            expiresIn: '12h'
+                        });
+
+                        res.json({
+                            token: token,
+                            user: login.usuario.nombres + ' ' + login.usuario.apellidos,
+                            msg: "Bienvenid@ " + login.usuario.nombres + ' ' + login.usuario.apellidos,
+                            usuario: login.user,
+                            external_id: login.usuario.external_id,
+                            code: 200
+                        });
+                    } else {
+                        res.json({
+                            msg: "CLAVE INCORRECTA",
+                            code: 201
+                        });
+                    }
+                } else if (login.estado == false) {
+                    res.json({
+                        msg: "CUENTA NO SE ENCUENTRA ACTIVA",
+                        code: 201
+                    });
+                } else {
+                    res.json({
+                        msg: "NO EXISTE ESA CUENTA",
+                        code: 201
+                    });
+                }
+            }
+        } else {
+            res.status(400);
+            res.json({ msg: "Datos faltantes", code: 400, errors: errors });
+        }
+    }
+    
+}
+module.exports = CuentaController;
--- backend_sistema_notificaciones/routes/users.js
+++ backend_sistema_notificaciones/routes/users.js
@@ -1,9 +1,70 @@
 var express = require('express');
 var router = express.Router();
 
+let jwt = require('jsonwebtoken');
+
+const { body, validationResult } = require('express-validator');
+
+const UsuarioController = require('../app/controls/UsuarioController');
+var usuarioController = new UsuarioController();
+const CuentaController = require('../app/controls/CuentaController');
+var cuentaController = new CuentaController();
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+var auth = function middleware(req, res, next) {
+  const token = req.headers['x-api-token'];
+  if (token) {
+    require('dotenv').config();
+    const llave = process.env.KEY_SQ;
+
+    jwt.verify(token, llave, async (err, decoded) => {
+      if (err) {
+        res.status(401);
+        res.json({
+          msg: "TOKEN NO VALIDO",
+          code: 401
+        });
+      } else {
+        var models = require('../models');
+        var cuenta = models.cuenta;
+        req.decoded = decoded;
+        let aux = await cuenta.findOne({ where: { external_id: req.decoded.external } })
+        if (aux === null) {
+          res.status(401);
+          res.json({
+            msg: "TOKEN NO VALIDO O EXPIRADO",
+            code: 401
+          });
+        } else {
+          next();
+        }
+      }
+    });
+  } else {
+    res.status(401);
+    res.json({
+      msg: "NO EXISTE TOKEN",
+      code: 401
+    });
+  }
+
+}
+
+/*-----------------------------------------------------------------RUTAS------------------------------------------------------------------*/
+
+/*CUENTA CONTROLLER */
+router.post('/cuenta/sesion', [
+  body('user', 'Ingrese un usuario').trim().exists().not().isEmpty(),
+  body('clave', 'Ingrese una clave').trim().exists().not().isEmpty(),
+], cuentaController.sesion);
+
+/*PERSONA CONTROLLER*/
+router.post('/persona/registrar', usuarioController.guardar);
+router.post('/persona/modificar', usuarioController.modificar);
+router.get('/persona/obtener/:external', usuarioController.obtener);
 
 module.exports = router;
